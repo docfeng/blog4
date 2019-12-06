c097af56fe7e038a2543373e62c187e9a0265fe0
docfeng <38637196+docfeng@users.noreply.github.com>
2019-12-06 12:51:37 -0600

    add

--- js/core2.js
+++ js/core2.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     //var repos_name=user + '.github.io';
     var repos_name="blog4";
     //user = 'yanghanqing';
-    blogListURL = 'https://api.github.com/repos/' + user + '/' + repos_name + '/contents/';
+    blogListURL = 'https://api.github.com/repos/' + user + '/' + repos_name + '/contents/blog';
     alert(1)
     if(!location.pathname.substr(-5).match(/\./)){
         blogListURL = 'https://api.github.com/repos/' + user + '/' + repos_name + '/contents'+location.pathname.replace(/^(\/)|(\/)$/g, '');
